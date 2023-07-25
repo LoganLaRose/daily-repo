abff18c431e2516e6173d7957f4a825e38c668eb
LoganLaRose <laroselogan@gmail.com>
2023-07-25 19:19:12 -0400

    second

--- problems/13-min-value-callback.js
+++ problems/13-min-value-callback.js
@@ -11,7 +11,11 @@ console.log(minValueCallback([64, 25, 49, 9, 100], Math.sqrt));  // 3
 *******************************************************************************/
 
 function minValueCallback(array, cb) {
-    // Your code here
+  if (cb) {
+    return Math.min(...array.map(cb));
+  } else {
+    return Math.min(...array);
+  }
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
